4e2fc7acc1273442e5504843dee0534903e94095
rafael.monteiro <rafael.monteiro@segfy.com>
2023-03-30 08:54:09 -0300

    Init of validations

---
+++ src/DailyCashFlowControl.Domain/Models/Requests/TransactionRequest.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace DailyCashFlowControl.Domain.Models.Requests
+{
+    public record TransactionRequest(string? Type, decimal? Value);
+
+    public class TransactionRequestValidator : AbstractValidator<TransactionRequest>
+    {
+        public TransactionRequestValidator()
+        {
+            RuleFor(x => x.Type).NotEmpty();
+            RuleFor(x => x.Value).NotNull();
+        }
+    }
+
+}
--- src/DailyCashFlowControl.Transactions.Api/Program.cs
+++ src/DailyCashFlowControl.Transactions.Api/Program.cs
@@ -1,10 +1,15 @@
 using DailyCashFlowControl.Application.Handlers;
 using DailyCashFlowControl.Domain.Interfaces;
-using DailyCashFlowControl.Domain.Models;
+using DailyCashFlowControl.Domain.Models.Requests;
 using DailyCashFlowControl.Main;
 using MediatR;
-using Microsoft.Extensions.DependencyInjection;
+using FluentValidation;
 using System.Reflection;
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using FluentValidation.Results;
+using System;
+using System.ComponentModel.DataAnnotations;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +21,7 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddRabbitMQ();
 builder.Services.AddMessageProducer();
+builder.Services.AddValidatorsFromAssemblyContaining<TransactionRequestValidator>();
 
 
 var app = builder.Build();
@@ -58,12 +64,31 @@ app.MapGet("/weatherforecast", () =>
 .WithOpenApi();
 
 
-app.MapPost("/todoitems", async (Transaction transaction, IMessageProducer _messagePublisher) =>
+app.MapPost("/transaction", async (TransactionRequest transaction, IValidator <TransactionRequest> validator,  IMessageProducer _messagePublisher) =>
 {
-    _messagePublisher.SendMessage(transaction);
+    //_messagePublisher.SendMessage(transaction);
 
-    return Results.Created($"/todoitems/{transaction.Id}", transaction);
+    var validationResult = validator.Validate(transaction);
+
+    if (validationResult.IsValid)
+    {
+        // do the thing
+        return Results.Ok();
+    }
+
+    return Results.ValidationProblem(validationResult.ToDictionary(),
+        statusCode: (int)HttpStatusCode.UnprocessableEntity);
 });
+
+//app.MapPost("/person", (Validated<Person> req) =>
+//{
+//    // deconstruct to bool & Person
+//    var (isValid, value) = req;
+
+//    return isValid
+//        ? Results.Ok(value)
+//        : Results.ValidationProblem(req.Errors);
+//});
 
 app.Run();
 
@@ -71,3 +96,17 @@ internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
+
+public record Person(string? Name, int? Age);
+
+// ReSharper disable once UnusedType.Global
+public class PersonValidator : AbstractValidator<Person>
+{
+    public PersonValidator()
+    {
+        RuleFor(m => m.Name).NotEmpty();
+        RuleFor(m => m.Age).NotEmpty().GreaterThan(0);
+    }
+}
+
+
